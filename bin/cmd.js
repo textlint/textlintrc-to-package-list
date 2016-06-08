79b6d51ef7575e729759102c83f75f7a00f81d20
azu <azuciao@gmail.com>
2016-06-08 23:37:15 +0900

    fix(bin): support comment in JSON

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 var listPackageName = require('../');
 var concat = require('concat-stream');
+const stripJsonComments = require("strip-json-comments");
+
 var fs = require('fs');
 var file = process.argv[2];
 var input = file && file !== '-'
     ? fs.createReadStream(process.argv[2])
     : process.stdin;
 input.pipe(concat(function (buf) {
-    var jsonData = JSON.parse(buf.toString('utf8'));
+    var jsonData = JSON.parse(stripJsonComments(buf.toString('utf8')));
     console.log(listPackageName(jsonData).join("\n"));
 }));
