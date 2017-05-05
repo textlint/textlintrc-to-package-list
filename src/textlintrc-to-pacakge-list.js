04d08446917007f0f98d6c02d94ac38a04882fc5
azu <azuciao@gmail.com>
2017-05-05 11:36:23 +0900

    fix: support full path of modules
    e.g.) textlint-filter-rule-whitelist

--- src/textlintrc-to-pacakge-list.js
+++ src/textlintrc-to-pacakge-list.js
@@ -11,13 +11,13 @@ export default function listPackageName(configJSON) {
     const rules = configJSON["rules"] || {};
     const filterRules = configJSON["filters"] || {};
     const pluginsNameList = plugins.map(key => {
-        return `textlint-plugin-${key}`;
+        return `textlint-plugin-${key.replace(/^textlint-plugin-/, "")}`;
     });
     const ruleNameList = Object.keys(rules).filter(validRulePackageKey).map(key => {
-        return `textlint-rule-${key}`;
+        return `textlint-rule-${key.replace(/textlint-rule-/, "")}`;
     });
     const filterRuleNameList = Object.keys(filterRules).filter(validRulePackageKey).map(key => {
-        return `textlint-filter-rule-${key}`;
+        return `textlint-filter-rule-${key.replace(/^textlint-filter-rule-/, "")}`;
     });
     return [].concat(pluginsNameList, filterRuleNameList, ruleNameList);
 }
--- test/textlintrc-to-pacakge-list-test.js
+++ test/textlintrc-to-pacakge-list-test.js
@@ -1,22 +1,23 @@
 // LICENSE : MIT
 "use strict";
-import assert from "power-assert"
 import fs from "fs";
+import path from "path";
+import assert from "assert"
 import listPackageNames from "../src/textlintrc-to-pacakge-list";
-describe("textlintrc-to-pacakge-list", function () {
-    it("should return package name list", function () {
-        var config = require("./fixtures/.textlintrc.json");
-        var names = listPackageNames(config);
-        assert.deepEqual(names, [
-            "textlint-plugin-jtf-style",
-            "textlint-filter-rule-comments",
-            "textlint-rule-max-ten",
-            "textlint-rule-no-doubled-joshi",
-            "textlint-rule-sentence-length",
-            "textlint-rule-no-start-duplicated-conjunction",
-            "textlint-rule-spellcheck-tech-word",
-            "textlint-rule-no-mix-dearu-desumasu",
-            "textlint-rule-prh"
-        ]);
+function normalize(str) {
+    return str.replace(/^\s+|\s+$/, '').replace(/\r?\n/g, '\n');
+}
+const fixturesDir = path.join(__dirname, 'fixtures');
+describe("textlintrc-to-pacakge-list", function() {
+    fs.readdirSync(fixturesDir).map((caseName) => {
+        it(`should generate assertions for ${caseName.replace(/-/g, ' ')}`, () => {
+            const fixtureDir = path.join(fixturesDir, caseName);
+            const actualPath = path.join(fixtureDir, '.textlintrc.json');
+            const expectedPath = path.join(fixtureDir, 'output.json');
+            const config = require(actualPath);
+            const names = listPackageNames(config);
+            const expected = require(expectedPath);
+            assert.deepEqual(names, expected);
+        });
     });
 });
